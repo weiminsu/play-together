909d0d75e8fcfbba570ff44cce0a9bf310d27213
weiminsu <s3794779@student.rmit.edu.au>
2022-04-05 21:09:55 +1000

    some UI improvements

--- app/src/main/resources/data.sql
+++ app/src/main/resources/data.sql
@@ -2,3 +2,8 @@ INSERT INTO ACCOUNTS VALUES (
 'b7ed93e6-250c-448f-ae22-f10b7069c289', 'tom', 'tom@abc.com', 'adjfasfe', 'its me mario', 'vermont south', ''
 )
 ON CONFLICT DO NOTHING;
+
+INSERT INTO CHILDREN VALUES
+('4af72e54-42c0-44cf-9b70-be02d6ae3447', 'b7ed93e6-250c-448f-ae22-f10b7069c289', 'Aron', 3),
+('cd9976e2-feff-4f1f-a732-b30d02ea2f8e', 'b7ed93e6-250c-448f-ae22-f10b7069c289', 'Brooke', 0)
+ON CONFLICT DO NOTHING;
--- frontend-app/src/profile/ProfilePage.js
+++ frontend-app/src/profile/ProfilePage.js
@@ -4,7 +4,6 @@ import { useParams } from "react-router-dom";
 
 export function withRouter(Children){
      return(props)=>{
-
         const match  = {params: useParams()};
         return <Children {...props}  match = {match}/>
     }
@@ -56,13 +55,48 @@ class ProfilePage extends React.Component {
                 <div id="profileImageDiv">
                       <img id="profileImage" src={account.profileImageUrl || process.env.PUBLIC_URL + "/default_profile_image.jpeg"} alt="No Profile Image" />
                 </div>
-                <div id="suburbDiv">
+                <div id="suburbDiv" class="form-floating mb-3">
+                      <input type="text" id="suburb" class="form-control" value={account.suburb} placeholder="Enter a suburb"></input>
                       <label for="suburb">Suburb: </label>
-                      <input type="text" id="suburb" value={account.suburb}></input>
                 </div>
-                <div id="descriptionDiv">
+                <div id="descriptionDiv" class="form-floating mb-3">
+                      <textarea id="description" class="form-control" value={account.description} placeholder="Enter some description"></textarea>
                       <label for="description">Description: </label>
-                      <textarea id="description" value={account.description}></textarea>
+                </div>
+                <div id="childrenDiv">
+                    <ul class="list-group child-list">
+                        {
+                            account.children.map( (child) => {
+                                return (
+                                    <li class="list-group-item">
+                                        <div class="card childCard">
+                                          <div class="card-body">
+                                            <h5 class="card-title">{child.name}</h5>
+                                            <h6 class="card-subtitle mb-2 text-muted">{child.age} year old</h6>
+                                            <p class="card-text">Some description</p>
+                                          </div>
+                                        </div>
+                                    </li>
+                                )
+                            })
+                        }
+                        <li class="list-group-item">
+                            <div class="card childCard">
+                              <div class="card-body">
+                                <h5 class="card-title">Add a new child</h5>
+                                <div class="form-floating mb-3">
+                                      <input type="text" id="add-a-child-name" class="form-control" placeholder="Child Name"></input>
+                                      <label for="add-a-child-name">Child Name: </label>
+                                </div>
+                                <div class="form-floating mb-3">
+                                       <input type="text" id="add-a-child-age" class="form-control" placeholder="Child Age"></input>
+                                      <label for="add-a-child">Child Age: </label>
+                                </div>
+                                <a href="#" class="btn btn-primary">Add</a>
+                              </div>
+                            </div>
+                        </li>
+                    </ul>
                 </div>
             </div>
         );
